684c6bfd4348b64f0e9d04749c024aa88ca77256
zachgalbs <zacharythomasgalbraith@gmail.com>
2022-11-25 18:01:11 -0800

    cleaned up the onclick function

--- javascript.js
+++ javascript.js
@@ -1,9 +1,5 @@
 let button = document.getElementById("workoutButton");
 
-function checkIfClick() {
-    button.onclick = function() {
-        console.log("button has been clicked!");
-    }
+button.onclick = function() {
+    console.log("button has been clicked!");
 }
-
-checkIfClick()
