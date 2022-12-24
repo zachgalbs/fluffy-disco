8ee5d7a16ce1cf7d93ef6b01d812aad179879de0
zachgalbs <zacharythomasgalbraith@gmail.com>
2022-12-24 12:09:00 -0800

    added the workout list

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let button = document.getElementById("workoutButton");
+let workoutList = document.getElementById("workoutList");
 
 button.onclick = function() {
     console.log("button has been clicked!");
+    workoutList.style.opacity = '1';
 }
